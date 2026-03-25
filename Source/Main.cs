d66093ed81d684f228c0ec36ebf2e77fff4bb12e
progmee <demidovalex2005@gmail.com>
2026-03-25 19:20:29 +0100

    Add project assembly

---
+++ Source/Main.cs
@@ -0,0 +1,14 @@
+using RimWorld;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+using Microsoft.Data.Sqlite;
+
+namespace RimStats {
+    [StaticConstructorOnStartup]
+    static class Main {
+        static Main() {
+            Log.Message("Hello world!");
+        }
+    }
+}
